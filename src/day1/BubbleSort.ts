51af70e38a6791a08edfa8acf197c9d000a24db3
Shivendra Bind <shivendra.ind@gmail.com>
2023-08-15 17:22:11 +0530

    BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    const l = arr.length;
+    for (let i = 0; i < l; i++) {
+        for (let j = 0; j < l - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                let t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+    }
+    console.log(arr);
 }
