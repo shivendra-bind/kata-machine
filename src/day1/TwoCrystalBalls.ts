e1190fcdd83fc21360fb520d09313b8befa07826
Shivendra Bind <shivendra.ind@gmail.com>
2023-08-15 16:55:24 +0530

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-  
+    const amtWalk = Math.floor(Math.sqrt(breaks.length))
+    let s = amtWalk
+
+    for(;s <breaks.length; s+=amtWalk){
+        if(breaks[s]){
+            break
+        }
+    }
+    for(let i= s-amtWalk; i<=s; i++){
+        if(breaks[i]){
+            return i
+        }
+    }
+    return -1
 }
