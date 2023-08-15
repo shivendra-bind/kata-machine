e0ab8643f893261ebbc9a53b3c1c04aa1946d848
Shivendra Bind <shivendra.ind@gmail.com>
2023-08-15 16:48:41 +0530

    BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    
+    let low = 0;
+    let hi = haystack.length;
+
+    if (low === hi) {
+        if (haystack[low] === needle) {
+            return true;
+        }
+        return false;
+    }
+    while (low < hi) {
+        let m = Math.floor(low + (hi - low) / 2);
+        const v = haystack[m];
+        if (v === needle) {
+            return true;
+        } else if (v < needle) {
+            low = m + 1;
+        } else {
+            hi = m;
+        }
+    }
+    return false;
 }
